54c25521714d447686d899d60570794415b10764
MaryKhm656 <nanoman66656@gmail.com>
2025-09-28 11:20:04 +0300

    Added logic in routes

--- app/web/main.py
+++ app/web/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
 
 from app.web import routes
@@ -9,6 +10,10 @@ def create_app() -> FastAPI:
     app = FastAPI(title="Task Scheduler", log_level="debug")
 
     app.mount("/static", StaticFiles(directory="static"), name="static")
+
+    @app.exception_handler(401)
+    async def unauthorized_handler(request: Request, exc: HTTPException):
+        return RedirectResponse(url="/", status_code=302)
 
     app.include_router(routes.router)
 
