72d99c4e3fe9ba2f12df4451d40f4a0c39d0c96e
MaryKhm656 <nanoman66656@gmail.com>
2025-09-28 17:33:00 +0300

    Added docker files

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
 
     uvicorn.run(
         "main:app",
-        host=os.getenv("HOST", "127.0.0.1"),
+        host=os.getenv("HOST", "0.0.0.0"),
         port=int(os.getenv("PORT", 8000)),
         reload=True,
     )
