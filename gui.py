0df1da21fe9ab056fb7f181f8deff6d3383761ff
MaryKhm656 <nanoman66656@gmail.com>
2025-10-05 16:54:07 +0300

    Edit gui

--- gui.py
+++ gui.py
@@ -4,12 +4,12 @@ from dotenv import load_dotenv
 load_dotenv()  # noqa: E402
 
 
-SERVER_URL = "http://194.39.101.101:8000/"
+SERVER_URL = "http://194.39.101.101:8000"
 
 if __name__ == "__main__":
     window = webview.create_window(
         "Планировщик задач",
-        SERVER_URL,
+        f"{SERVER_URL}/?gui=1",
         width=1500,
         height=900,
         min_size=(800, 600),
