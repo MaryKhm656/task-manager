7e71232bd1d12b95904c12e3a3822e7332023959
MaryKhm656 <nanoman66656@gmail.com>
2025-10-07 21:53:43 +0300

    Edit gui

--- app/web/main.py
+++ app/web/main.py
@@ -23,6 +23,18 @@ def create_app(is_gui: bool = False) -> FastAPI:
         request.state.is_gui = is_gui_flag
 
         response = await call_next(request)
+
+        if is_gui_flag and not request.cookies.get("is_gui"):
+            secure_flag = request.url.scheme == "https"
+            response.set_cookie(
+                key="is_gui",
+                value="1",
+                max_age=60 * 60 * 24 * 30,
+                httponly=True,
+                samesite="lax",
+                secure=secure_flag,
+            )
+
         return response
 
     @app.get("/gui-launch")
@@ -37,6 +49,12 @@ def create_app(is_gui: bool = False) -> FastAPI:
             samesite="lax",
             secure=secure_flag,
         )
+        return resp
+
+    @app.get("/web-launch")
+    async def web_launch(request: Request):
+        resp = RedirectResponse(url="/")
+        resp.delete_cookie("is_gui")
         return resp
 
     app.include_router(routes.router)
